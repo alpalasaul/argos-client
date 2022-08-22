318fc4059c88441b68d2fdddcda2858811320f93
alpalasaul <alpalasaul@gmail.com>
2022-08-21 19:18:32 -0500

    Live Stream agregado

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded">
       <div className="container flex flex-wrap justify-between items-center mx-auto">
-        <a href="https://flowbite.com/" className="flex items-center">
+        <a href="/" className="flex items-center">
           <img src="https://flowbite.com/docs/images/logo.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
           <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white uppercase">Helment Detection Cam</span>
         </a>
--- src/components/Streaming.jsx
+++ src/components/Streaming.jsx
@@ -3,10 +3,21 @@ import React from 'react'
 const Streaming = () => {
   return (
     <div>
-    <p className="text-lg mt-5 text-center mb-5">
-        Capturando Live Strem desde {''}
-        <span className="text-indigo-600 font-bold uppercase">camara IP</span>
-    </p>
+      <p className="text-lg mt-5 text-center mb-5">
+          Capturando Live Strem desde {''}
+          <span className="text-indigo-600 font-bold uppercase">camara IP</span>
+      </p>
+      <div className='flex justify-center'>
+        <iframe 
+          width="854" 
+          height="480" 
+          src="https://www.youtube.com/embed/36YnV9STBqc?controls=0&autoplay=1" 
+          title="YouTube video player" 
+          frameborder="0" 
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+          allowfullscreen
+        />
+      </div>
     </div>
   )
 }
