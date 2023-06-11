b0f1329de100ce3d8f1849aa1d269787c96ebc14
Saúl Alpala <69735886+alpalasaul@users.noreply.github.com>
2023-06-11 15:56:36 +0000

    Add skip ngrok warning message

--- src/utils/utilities.js
+++ src/utils/utilities.js
@@ -3,8 +3,17 @@ import axios from "axios";
 // const SERVER = "https://fair-eagles-play-34-74-178-248.loca.lt";
 const SERVER = "http://50.116.23.81";
 
+// Salta automaticamente el mensaje que sale con ngrok
+const axiosConfig = axios.create({
+  baseURL: SERVER,
+  Headers: {
+    "ngrok-skip-browser-warning": "true"
+  }
+})
+
 export const getAllCameras = async () => {
-  const response = await axios.get(SERVER + "/fetchAllCameras");
+  // TODO: revisar que funcione 
+  const response = await axiosConfig.get("/fetchAllCameras");
   return response.data;
 };
 
